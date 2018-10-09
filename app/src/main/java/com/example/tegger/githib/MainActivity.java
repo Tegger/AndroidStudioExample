69b4e9bd7908f3c52777e0d39772b13ffdf007be
Thomas Egger <tegger01@yahoo.com>
2018-10-09 10:36:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tegger/githib/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tegger.githib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO i need to do this
+    }
+}
